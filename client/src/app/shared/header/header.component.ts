15238b6f4754fea4de0f7c89b473621c0deed1f1
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-06-24 12:40:56 +0300

    #14 - From css to scss

--- client/src/app/shared/header/header.component.ts
+++ client/src/app/shared/header/header.component.ts
@@ -7,7 +7,7 @@ import { User } from "../models/user.model";
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
