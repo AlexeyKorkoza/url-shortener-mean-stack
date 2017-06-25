d758748445470bd38ddc1b6b38ab0dd3c09e9fee
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-06-25 15:03:22 +0300

    #3 - Create models

---
+++ client/src/app/shared/models/url.model.ts
@@ -0,0 +1,12 @@
+import { Tag } from "./tag.model";
+
+export class Url {
+    author: String;
+    description: String;
+    list_tags: Tag [];
+    count_click: Number;
+    full_url: String;
+    short_url: String;
+    date: String;
+    time: String;
+}
---
+++ server/models/url.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UrlSchema = new Schema({
+  author: String,
+  description: String,
+  list_tags: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Tag'
+  }],
+  count_click: {
+    type: Number,
+    default: 0
+  },
+  full_url: String,
+  short_url: String,
+  date: String,
+  time: String
+});
+
+var Url = mongoose.model('Url', UrlSchema);
+
+module.exports = Url;
